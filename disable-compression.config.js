b5334a05c0bc51ce911ea3a7f5e2a144cab1071a
Maksym <wfbykahdapaiii@gmail.com>
2022-10-03 15:09:18 +0300

    Add different config file

---
+++ disable-compression.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require("cypress");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
+async function setupNodeEvents(on, config) {
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+  on("file:preprocessor", browserify.default(config));
+  allureWriter(on, config);
+  return config;
+}
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: "**/*.feature",
+    videoCompression: false,
+    videosFolder: "cypress/videos/uncompressed-videos",
+    setupNodeEvents,
+    env: {
+      allureReuseAfterSpec: true
+    },
+  },
+});
